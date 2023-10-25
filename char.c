ba4bce0ab9e1f8ce4f9e52c422da32d6cf2a5909
Stanley <stanleyemeke71@gmail.com>
2023-10-24 22:26:03 -0700

    My first C practice

---
+++ char.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function to print the values of variables
+ * Return: void
+ */
+
+void main()
+{	
+	char a = 'a';
+	char b = 98;
+	char c = 'z';
+	char d = 130;
+	char e = 129;
+
+	printf("%c\n", a);
+	printf("%d\n", a);
+	printf("%d\n", b);
+	printf("%d\n", c);
+	printf("%c\n", d);
+	printf("%d\n", d);
+	printf("%d\n", e);
+}
