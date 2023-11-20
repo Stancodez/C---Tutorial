380f266740a1290c9e3e01a0c27a5daa5b4a624c
Stanley <stanleyemeke71@gmail.com>
2023-11-19 19:26:33 -0800

    Matrix

---
+++ CodePractice/2D-Array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print 2D array and calculate it sum
+ * Return : 0
+ */
+
+int main()
+{
+	int a[2][3], i, j, sum = 0;
+
+	printf("Enter elements of array: \n");
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			scanf("%d", &a[i][j]);
+		}
+	}
+
+	printf("Matrix is: \n");
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			printf("%d\t", a[i][j]);
+			sum = sum + a[i][j];
+		
+		}
+		printf("\n");
+	}
+	printf("\nSum is = %d", sum);	
+}
